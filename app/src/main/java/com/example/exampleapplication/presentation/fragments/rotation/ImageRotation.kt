4a99a94900daf800edcd8b614929610a94dba5d1
Lisandro Diaz Di Meo <ddmlisandro22@gmail.com>
2023-08-15 10:08:42 -0300

    Rotation indicator now rotates properly

--- app/src/main/java/com/example/exampleapplication/presentation/fragments/rotation/ImageRotation.kt
+++ app/src/main/java/com/example/exampleapplication/presentation/fragments/rotation/ImageRotation.kt
@@ -1,49 +1,33 @@
 package com.example.exampleapplication.presentation.fragments.rotation
 
+import android.util.Log
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.detectDragGestures
-import androidx.compose.foundation.gestures.draggable
-import androidx.compose.foundation.gestures.rememberDraggableState
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.center
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.drawscope.rotate
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.ExperimentalTextApi
-import androidx.compose.ui.text.PlatformTextStyle
-import androidx.compose.ui.text.TextLayoutInput
-import androidx.compose.ui.text.TextLayoutResult
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.drawText
 import androidx.compose.ui.text.rememberTextMeasurer
-import androidx.compose.ui.text.style.TextDirection
-import androidx.compose.ui.text.style.TextGeometricTransform
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.example.exampleapplication.R
+import androidx.compose.ui.unit.center
 import kotlin.math.abs
+import kotlin.math.atan2
 import kotlin.math.cos
+import kotlin.math.pow
 import kotlin.math.sin
+import kotlin.math.sqrt
 
 @OptIn(ExperimentalTextApi::class)
 @Preview(device = Devices.PIXEL_3A)
@@ -51,16 +35,48 @@ import kotlin.math.sin
 fun ImageRotation() {
     val goniometerBgColor = Color(0x4a00FFFF)
     val textMeasurer = rememberTextMeasurer()
+    var circleRadius by remember { mutableStateOf(1f) }
+    var circleCenter by remember { mutableStateOf(Offset.Zero) }
+    var dragStartedAngle by remember { mutableStateOf(0f) }
+    val coroutineScope = rememberCoroutineScope()
+    var shouldAcceptDrag by remember { mutableStateOf(false) }
+    var rotationAngle by remember { mutableStateOf(0f) }
+    var oldAngle by remember { mutableStateOf(rotationAngle) }
+
+
     Canvas(
         modifier = Modifier
             .fillMaxSize()
             .pointerInput(true) {
                 detectDragGestures(
                     onDragStart = { dragStart ->
-
+                        shouldAcceptDrag = isOnCircle(
+                            circleCenter,
+                            dragStart,
+                            circleRadius
+                        )
+                        dragStartedAngle = atan2(
+                            y = size.center.x - dragStart.x,
+                            x = size.center.y - dragStart.y
+                        ) * (180f / Math.PI.toFloat()) * -1
                     },
-                    onDrag = { change, dragAmount ->
-
+                    onDragEnd = {
+                        if(shouldAcceptDrag)
+                            oldAngle = rotationAngle
+                    },
+                    onDrag = { change, _ ->
+                        if(shouldAcceptDrag) {
+                            val touchAngle = atan2(
+                                y = size.center.x - change.position.x,
+                                x = size.center.y - change.position.y
+                            ) * (180f / Math.PI.toFloat()) * -1
+                            rotationAngle = oldAngle + (touchAngle - dragStartedAngle)
+                            if (rotationAngle > 360) {
+                                rotationAngle -= 360
+                            } else if (rotationAngle < 0) {
+                                rotationAngle = 360 - abs(rotationAngle)
+                            }
+                        }
                     }
                 )
             }
@@ -68,11 +84,22 @@ fun ImageRotation() {
 
         // The Main Circle of the Goniometer
         val goniometerRadius = (size.width * .5f) - (.25f * (size.width * .5f))
+        circleRadius = goniometerRadius
+        circleCenter = size.center
         drawCircle(
             color = goniometerBgColor,
             radius = goniometerRadius,
             center = size.center
         )
+
+        // Temporary Angle Indicator
+        rotate(rotationAngle, size.center) {
+            drawCircle(
+                color = Color.Red,
+                radius = goniometerRadius / 20,
+                center = Offset(x = center.x, y = center.y - (500f))
+            )
+        }
 
         // The Angles to select
         // Here we will use the polar system to mark each line
@@ -149,4 +176,12 @@ fun ImageRotation() {
         }
     }
 
+}
+
+private fun isOnCircle(circleCenter: Offset, point: Offset, radius: Float): Boolean {
+    val isOnCircle = sqrt(
+        abs((circleCenter.x - point.x)).pow(2) + abs((circleCenter.y - point.y)).pow(2)
+    ) <= radius
+    Log.i("MATH", "Is $point on circle with center $circleCenter and r=$radius? $isOnCircle")
+    return isOnCircle
 }
