636af2dc39ce5e10533b2ebf96413691d45b5d16
Lisandro Diaz Di Meo <ddmlisandro22@gmail.com>
2023-08-15 10:17:57 -0300

    Full rotate of main circle

--- app/src/main/java/com/example/exampleapplication/presentation/fragments/rotation/ImageRotation.kt
+++ app/src/main/java/com/example/exampleapplication/presentation/fragments/rotation/ImageRotation.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -32,17 +33,22 @@ import kotlin.math.sqrt
 @OptIn(ExperimentalTextApi::class)
 @Preview(device = Devices.PIXEL_3A)
 @Composable
-fun ImageRotation() {
+fun ImageRotation(
+    onAngleChanged: (Float) -> Unit = {
+        Log.i("IMAGE_ROTATE", it.toString())
+    }
+) {
     val goniometerBgColor = Color(0x4a00FFFF)
     val textMeasurer = rememberTextMeasurer()
     var circleRadius by remember { mutableStateOf(1f) }
     var circleCenter by remember { mutableStateOf(Offset.Zero) }
     var dragStartedAngle by remember { mutableStateOf(0f) }
-    val coroutineScope = rememberCoroutineScope()
     var shouldAcceptDrag by remember { mutableStateOf(false) }
     var rotationAngle by remember { mutableStateOf(0f) }
     var oldAngle by remember { mutableStateOf(rotationAngle) }
-
+    LaunchedEffect(rotationAngle) {
+        onAngleChanged(rotationAngle)
+    }
 
     Canvas(
         modifier = Modifier
@@ -94,86 +100,86 @@ fun ImageRotation() {
 
         // Temporary Angle Indicator
         rotate(rotationAngle, size.center) {
-            drawCircle(
-                color = Color.Red,
-                radius = goniometerRadius / 20,
-                center = Offset(x = center.x, y = center.y - (500f))
-            )
-        }
-
-        // The Angles to select
-        // Here we will use the polar system to mark each line
-        val angleUnit = 30
-        val subAngleUnit = 2
-        val anglesSeparators = 360 / angleUnit
-        val subAnglesSeparators = angleUnit / subAngleUnit
-        for (marker in 1..anglesSeparators) {
-            val angleInDegrees = angleUnit * marker
-            val startOffset = Offset(
-                x = size.center.x + .90f * goniometerRadius * cos(
-                    Math.toRadians(angleInDegrees.toDouble())
-                ).toFloat(),
-                y = size.center.y + .90f * goniometerRadius * sin(
-                    Math.toRadians(angleInDegrees.toDouble())
-                ).toFloat()
-            )
-            val endOffset = Offset(
-                x = size.center.x + 1.10f * goniometerRadius * cos(
-                    Math.toRadians(angleInDegrees.toDouble())
-                ).toFloat(),
-                y = size.center.y + 1.10f * goniometerRadius * sin(
-                    Math.toRadians(angleInDegrees.toDouble())
-                ).toFloat()
-            )
-            drawLine(
-                color = Color.Gray,
-                start = startOffset,
-                end = endOffset,
-                strokeWidth = 10f
-            )
-            val textOffset = Offset(
-                x = size.center.x + 1.25f * goniometerRadius * cos(
-                    Math.toRadians(angleInDegrees.toDouble())
-                ).toFloat(),
-                y = size.center.y + 1.25f * goniometerRadius * sin(
-                    Math.toRadians(angleInDegrees.toDouble())
-                ).toFloat()
-            )
-            rotate(angleInDegrees.toFloat() + 90f, textOffset) {
-                drawText(
-                    textMeasurer = textMeasurer,
-                    text = "$angleInDegrees",
-                    topLeft = textOffset
-                )
-            }
-
-
-            for (subMarker in 1..subAnglesSeparators) {
-                val subAngleInDegrees = angleInDegrees + subAngleUnit * subMarker
-                val subStartOffset = Offset(
+//            drawCircle(
+//                color = Color.Red,
+//                radius = goniometerRadius / 20,
+//                center = Offset(x = center.x, y = center.y - (500f))
+//            )
+            // The Angles to select
+            // Here we will use the polar system to mark each line
+            val angleUnit = 30
+            val subAngleUnit = 2
+            val anglesSeparators = 360 / angleUnit
+            val subAnglesSeparators = angleUnit / subAngleUnit
+            for (marker in 1..anglesSeparators) {
+                val angleInDegrees = angleUnit * marker
+                val startOffset = Offset(
                     x = size.center.x + .90f * goniometerRadius * cos(
-                        Math.toRadians(subAngleInDegrees.toDouble())
+                        Math.toRadians(angleInDegrees.toDouble())
                     ).toFloat(),
                     y = size.center.y + .90f * goniometerRadius * sin(
-                        Math.toRadians(subAngleInDegrees.toDouble())
+                        Math.toRadians(angleInDegrees.toDouble())
                     ).toFloat()
                 )
-                val subEndOffset = Offset(
-                    x = size.center.x + goniometerRadius * cos(
-                        Math.toRadians(subAngleInDegrees.toDouble())
+                val endOffset = Offset(
+                    x = size.center.x + 1.10f * goniometerRadius * cos(
+                        Math.toRadians(angleInDegrees.toDouble())
                     ).toFloat(),
-                    y = size.center.y + goniometerRadius * sin(
-                        Math.toRadians(subAngleInDegrees.toDouble())
+                    y = size.center.y + 1.10f * goniometerRadius * sin(
+                        Math.toRadians(angleInDegrees.toDouble())
                     ).toFloat()
                 )
                 drawLine(
                     color = Color.Gray,
-                    start = subStartOffset,
-                    end = subEndOffset,
-                    strokeWidth = 5f
+                    start = startOffset,
+                    end = endOffset,
+                    strokeWidth = 10f
                 )
+                val textOffset = Offset(
+                    x = size.center.x + 1.25f * goniometerRadius * cos(
+                        Math.toRadians(angleInDegrees.toDouble())
+                    ).toFloat(),
+                    y = size.center.y + 1.25f * goniometerRadius * sin(
+                        Math.toRadians(angleInDegrees.toDouble())
+                    ).toFloat()
+                )
+                rotate(angleInDegrees.toFloat() + 90f, textOffset) {
+                    drawText(
+                        textMeasurer = textMeasurer,
+                        text = "$angleInDegrees",
+                        topLeft = textOffset
+                    )
+                }
+                
+                for (subMarker in 1..subAnglesSeparators) {
+                    val subAngleInDegrees = angleInDegrees + subAngleUnit * subMarker
+                    val subStartOffset = Offset(
+                        x = size.center.x + .90f * goniometerRadius * cos(
+                            Math.toRadians(subAngleInDegrees.toDouble())
+                        ).toFloat(),
+                        y = size.center.y + .90f * goniometerRadius * sin(
+                            Math.toRadians(subAngleInDegrees.toDouble())
+                        ).toFloat()
+                    )
+                    val subEndOffset = Offset(
+                        x = size.center.x + goniometerRadius * cos(
+                            Math.toRadians(subAngleInDegrees.toDouble())
+                        ).toFloat(),
+                        y = size.center.y + goniometerRadius * sin(
+                            Math.toRadians(subAngleInDegrees.toDouble())
+                        ).toFloat()
+                    )
+                    drawLine(
+                        color = Color.Gray,
+                        start = subStartOffset,
+                        end = subEndOffset,
+                        strokeWidth = 5f
+                    )
+                }
             }
         }
+
+
     }
 
 }
