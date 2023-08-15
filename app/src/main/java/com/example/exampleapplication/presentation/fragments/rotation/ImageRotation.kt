449ebc9a75a8166e7cd4b04d90b612bc195e7d56
Lisandro Diaz Di Meo <ddmlisandro22@gmail.com>
2023-08-15 11:15:15 -0300

    Added example image to rotate?

--- app/src/main/java/com/example/exampleapplication/presentation/fragments/rotation/ImageRotation.kt
+++ app/src/main/java/com/example/exampleapplication/presentation/fragments/rotation/ImageRotation.kt
@@ -2,9 +2,11 @@ package com.example.exampleapplication.presentation.fragments.rotation
 
 import android.util.Log
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -13,18 +15,23 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.center
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.drawscope.rotate
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.ExperimentalTextApi
 import androidx.compose.ui.text.drawText
 import androidx.compose.ui.text.rememberTextMeasurer
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.center
+import androidx.compose.ui.unit.dp
+import com.example.exampleapplication.R
 import kotlin.math.abs
 import kotlin.math.atan2
 import kotlin.math.cos
@@ -60,7 +67,7 @@ fun ImageRotation(
 
     Canvas(
         modifier = Modifier
-            .fillMaxSize()
+            .size(512.dp)
             .pointerInput(true) {
                 detectDragGestures(
                     onDragStart = { dragStart ->
@@ -154,7 +161,7 @@ fun ImageRotation(
                 rotate(angleInDegrees.toFloat() + 90f, textOffset) {
                     drawText(
                         textMeasurer = textMeasurer,
-                        text = "$angleInDegrees",
+                        text = "${angleInDegrees % 360}",
                         topLeft = textOffset
                     )
                 }
@@ -196,13 +203,21 @@ fun ImageRotation(
 @Composable
 fun RotationPreview() {
     var angle by remember { mutableStateOf("") }
+    var rotationAngle by remember { mutableStateOf(0f) }
     Column(Modifier.fillMaxSize()) {
         Text(angle)
         ImageRotation {
             val absoluteAngle = (270 - it.roundToInt()) % 360
             val finalAngle = if (absoluteAngle < 0) (absoluteAngle + 360) % 360 else absoluteAngle
+            rotationAngle = finalAngle.toFloat()
             angle = finalAngle.toString() + "º (${it.roundToInt()})"
         }
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_foreground),
+            contentDescription = null,
+            modifier = Modifier.rotate(rotationAngle),
+            colorFilter = ColorFilter.tint(Color.Red)
+        )
     }
 }
 
